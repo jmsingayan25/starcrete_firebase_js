54a82ca18fce4da1061aef4ffb4b8926ec39636a
John Michael Singayan <jsingayan54@gmail.com>
2018-05-18 15:46:13 +0800

    login and create user
    function for adding, signing, update user details, load logged in user info

---
+++ js_config/firebase_config.js
@@ -0,0 +1,13 @@
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCo9ZmFHKVof67xkCFblIlEE0U9FqPbWrg",
+        authDomain: "starcrete-ff12d.firebaseapp.com",
+        databaseURL: "https://starcrete-ff12d.firebaseio.com",
+        projectId: "starcrete-ff12d",
+        storageBucket: "starcrete-ff12d.appspot.com",
+        messagingSenderId: "4339267998"
+    };
+
+    firebase.initializeApp(config);
+
