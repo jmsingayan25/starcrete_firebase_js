b4784250782e873e686a6a6cf26abc859d3631d8
John Michael Singayan <jsingayan54@gmail.com>
2018-07-07 17:00:14 +0800

    new config

--- js_config/firebase_config.js
+++ js_config/firebase_config.js
@@ -9,5 +9,14 @@
         messagingSenderId: "4339267998"
     };
 
+    // var config = {
+    //     apiKey: "AIzaSyB3Hj--oag7a9UvPhkmEeOXeyyyjk_N4Wo",
+    //     authDomain: "starcretebackup.firebaseapp.com",
+    //     databaseURL: "https://starcretebackup.firebaseio.com",
+    //     projectId: "starcretebackup",
+    //     storageBucket: "starcretebackup.appspot.com",
+    //     messagingSenderId: "528972955374"
+    // };
+
     firebase.initializeApp(config);
 
