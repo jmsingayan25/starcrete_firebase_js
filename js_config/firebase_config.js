7b1cbe121cc0ec1ceed2e142a37f3ce830649865
dmj0shua1 <dmj0shua1@gmail.com>
2018-07-09 14:38:03 +0800

    update

--- js_config/firebase_config.js
+++ js_config/firebase_config.js
@@ -1,22 +1,22 @@
 
     // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyCo9ZmFHKVof67xkCFblIlEE0U9FqPbWrg",
-        authDomain: "starcrete-ff12d.firebaseapp.com",
-        databaseURL: "https://starcrete-ff12d.firebaseio.com",
-        projectId: "starcrete-ff12d",
-        storageBucket: "starcrete-ff12d.appspot.com",
-        messagingSenderId: "4339267998"
-    };
-
     // var config = {
-    //     apiKey: "AIzaSyB3Hj--oag7a9UvPhkmEeOXeyyyjk_N4Wo",
-    //     authDomain: "starcretebackup.firebaseapp.com",
-    //     databaseURL: "https://starcretebackup.firebaseio.com",
-    //     projectId: "starcretebackup",
-    //     storageBucket: "starcretebackup.appspot.com",
-    //     messagingSenderId: "528972955374"
+    //     apiKey: "AIzaSyCo9ZmFHKVof67xkCFblIlEE0U9FqPbWrg",
+    //     authDomain: "starcrete-ff12d.firebaseapp.com",
+    //     databaseURL: "https://starcrete-ff12d.firebaseio.com",
+    //     projectId: "starcrete-ff12d",
+    //     storageBucket: "starcrete-ff12d.appspot.com",
+    //     messagingSenderId: "4339267998"
     // };
+
+    var config = {
+        apiKey: "AIzaSyB3Hj--oag7a9UvPhkmEeOXeyyyjk_N4Wo",
+        authDomain: "starcretebackup.firebaseapp.com",
+        databaseURL: "https://starcretebackup.firebaseio.com",
+        projectId: "starcretebackup",
+        storageBucket: "starcretebackup.appspot.com",
+        messagingSenderId: "528972955374"
+    };
 
     firebase.initializeApp(config);
 
